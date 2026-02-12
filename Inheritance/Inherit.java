cfd3ccecc39c23ee347c67acfbfcd989b4d1ee06
Samir Deshmukh <samirdesh220@gmail.com>
2026-02-12 11:09:18 +0530

    Inheritance

---
+++ Inheritance/Inherit.java
@@ -0,0 +1,26 @@
+//Java support single,Multilevel & Hierarchical inheritance
+
+
+class Parent{
+static String name="Samir";
+}
+
+ class Child extends Parent{
+   public Child(){
+    System.out.println(name);
+   }
+    }
+
+
+
+
+public class Inherit{
+    public static void main(String[] args) {
+        
+        //child inherit from parent
+      
+    new Child();
+
+      
+    }
+}
